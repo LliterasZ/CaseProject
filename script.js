4dd4b55811e83aed3f2b50cd28d3300b56b6b457
mkroth93 <46752738+mkroth93@users.noreply.github.com>
2019-09-16 16:46:01 -0500

    finished loop

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 "use strict";
 
 var pictures = ["try.jpg", "wut.jpg", "muffin.jpg", "thumbsUp.jpg"];
+
+<<<<<<< HEAD
+
+for (var pic of pictures) {
+    var img = document.createElement("img");
+    img.src = pic;
+    img.onclick = function() { document.getElementById('mainPic').src = this.src; };
+    document.getElementById('thumbnail').appendChild(img);
+}
+=======
+for (var pic of "pictures")
+{ 
+	
+}
+>>>>>>> 78f1c1d5ee75f908a41c9622d521a9e6068f0976
